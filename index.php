078e612988344e8a4ed757b743d1751c5f13dc7f
dxint <dxint@OPTIMUS>
2015-09-17 05:07:38 -0300

    76/09/15 limpieza

--- index.php
+++ index.php
@@ -1,107 +1,107 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-<title>Portal Estudiantil Jumbo Rancagua</title>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="icon" type="image/png" href="images/logo.png" />
-<link rel="stylesheet" href="assets/css/main.css" />
+    <head>
+        <title>Portal Estudiantil Jumbo Rancagua</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="images/logo.png" />
+        <link rel="stylesheet" href="assets/css/main.css" />
 
-</head>
-<body class="landing">
-<div id="page-wrapper"> 
-  
-  <!-- Header -->
-  <header id="header" class="alt">
-    <h1 id="logoo"><img width="56" height="56" src="images/logo.png" /></h1>
-    <nav id="nav">
-      <ul>
-        <li><a href="index.html"><img width="20" height="20" src="images/home.png" /> Home</a></li>
-        <li> <a href="#" class="icon fa-angle-down">= Menu</a>
-          <ul>
-            <li><a href="tomadeturnos.html">Toma de Turnos</a></li>
-            <li><a href="download.html">Descargar&nbsp Planilla</a></li>
-            <li><a href="contact.html">Regalar T. 17:30/19:00</a></li>
-            <li><a href="#">Cuenta.</a></li>
-            <li><a href="#">Contact</a></li>
-            
-            <li> <a href="#">Gestionar</a>
-              <ul>
-                <li><a href="#">Administrar</a></li>
-                <li><a href="#">Suspender</a></li>
-                <li><a href="#">Bloqueados</a></li>
-                <li><a href="redimencionado.html">Redimencionar</a></li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li><a href="https://www.google.com/" class="button">Salir&nbsp (Logout)</a></li>
-      </ul>
-    </nav>
-  </header>
-  
-  <!-- Banner -->
-  <section id="banner">
-  
-    <div class="container mlogin">
-      <div id="login">
-        <h2>PortalEstudiantil.cl</h2>
-        <form name="loginform" id="loginform" action="tomadeturnos.html" method="POST">
-        <p>Inicia sesión con tu - Cuenta de Correo.</p>
-       
-          <p>
-            
-              <input type="email"  name="username" id="username" class="input" value="" maxlength="113" placeholder="Correo electrónico o número de teléfono"  />
-            <br/>
-          </p>
-          <p>
-            
-              <input type="password" name="password" id="password" class="input" value=""   placeholder="Contraseña" />
-            
-        </p>
-        
+    </head>
+    <body class="landing">
+        <div id="page-wrapper"> 
 
-           <ul class="actions" >
-           <li> <input type="submit" name="login" class="button" value="Entrar" /></li>
-           <li><a href="#" class="button">¿Olvidaste tu contraseña?</a></li>
-          </ul>
-        </form>
-      </div>
-    </div>
-    
-    
-    
-  </section>
-  
-  <!-- Main --> 
-  
-  <!-- CTA -->
-  <section id="cta"></section>
-  
-  <!-- Footer -->
-  <footer id="footer">
-    <ul class="icons">
-      <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-      <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-      <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-      <li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
-      <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-      <li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
-    </ul>
-    <ul class="copyright">
-      <li>&copy; Dxintro. All rights reserved.</li>
-      <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-    </ul>
-  </footer>
-</div>
+            <!-- Header -->
+            <header id="header" class="alt">
+                <h1 id="logoo"><img width="56" height="56" src="images/logo.png" /></h1>
+                <nav id="nav">
+                    <ul>
+                        <li><a href="index.html"><img width="20" height="20" src="images/home.png" /> Home</a></li>
+                        <li> <a href="#" class="icon fa-angle-down">= Menu</a>
+                            <ul>
+                                <li><a href="tomadeturnos.html">Toma de Turnos</a></li>
+                                <li><a href="download.html">Descargar&nbsp Planilla</a></li>
+                                <li><a href="contact.html">Regalar T. 17:30/19:00</a></li>
+                                <li><a href="#">Cuenta.</a></li>
+                                <li><a href="#">Contact</a></li>
+
+                                <li> <a href="#">Gestionar</a>
+                                    <ul>
+                                        <li><a href="#">Administrar</a></li>
+                                        <li><a href="#">Suspender</a></li>
+                                        <li><a href="#">Bloqueados</a></li>
+                                        <li><a href="redimencionado.html">Redimencionar</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="https://www.google.com/" class="button">Salir&nbsp (Logout)</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <!-- Banner -->
+            <section id="banner">
+
+                <div class="container mlogin">
+                    <div id="login">
+                        <h2>PortalEstudiantil.cl</h2>
+                        <form name="loginform" id="loginform" action="tomadeturnos.html" method="POST">
+                            <p>Inicia sesión con tu - Cuenta de Correo.</p>
+
+                            <p>
+
+                                <input type="email"  name="username" id="username" class="input" value="" maxlength="113" placeholder="Correo electrónico o número de teléfono"  />
+                                <br/>
+                            </p>
+                            <p>
+
+                                <input type="password" name="password" id="password" class="input" value=""   placeholder="Contraseña" />
+
+                            </p>
 
 
-<script src="assets/js/jquery.min.js"></script> 
-<script src="assets/js/jquery.dropotron.min.js"></script> 
-<script src="assets/js/jquery.scrollgress.min.js"></script> 
-<script src="assets/js/skel.min.js"></script> 
-<script src="assets/js/util.js"></script> 
-<script src="assets/js/main.js"></script>
+                            <ul class="actions" >
+                                <li> <input type="submit" name="login" class="button" value="Entrar" /></li>
+                                <li><a href="#" class="button">¿Olvidaste tu contraseña?</a></li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
 
-</body>
+
+
+            </section>
+
+            <!-- Main --> 
+
+            <!-- CTA -->
+            <section id="cta"></section>
+
+            <!-- Footer -->
+            <footer id="footer">
+                <ul class="icons">
+                    <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                    <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                    <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                    <li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+                    <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+                    <li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+                </ul>
+                <ul class="copyright">
+                    <li>&copy; Dxintro. All rights reserved.</li>
+                    <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                </ul>
+            </footer>
+        </div>
+
+
+        <script src="assets/js/jquery.min.js"></script> 
+        <script src="assets/js/jquery.dropotron.min.js"></script> 
+        <script src="assets/js/jquery.scrollgress.min.js"></script> 
+        <script src="assets/js/skel.min.js"></script> 
+        <script src="assets/js/util.js"></script> 
+        <script src="assets/js/main.js"></script>
+
+    </body>
 </html>
